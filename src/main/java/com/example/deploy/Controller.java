1a30a0ebe20d7e0517aa19d67ffdd438bd0042ea
seung-woo-ryu <tmddn645@naver.com>
2024-10-13 16:27:50 +0900

    init2

---
+++ src/main/java/com/example/deploy/Controller.java
@@ -0,0 +1,14 @@
+package com.example.deploy;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class Controller {
+    @GetMapping
+    public String test() {
+        return "test!!!";
+    }
+}
